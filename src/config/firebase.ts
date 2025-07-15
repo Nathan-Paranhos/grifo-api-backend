9975e8506a9760a21181e7403cbc36a39824560e
Nathan <paranhoscontato.n@gmail.com>
2025-07-15 00:06:20 -0300

    feat: atualizar configuração do Firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 import 'dotenv/config';
 import logger from './logger';
-import * as path from 'path';
 
 let db: admin.firestore.Firestore | undefined;
 let firebaseInitialized = false;
@@ -14,20 +13,29 @@ export const initializeFirebase = (): Promise<admin.firestore.Firestore> => {
 
     try {
       if (admin.apps.length === 0) {
-        const serviceAccountPath = path.resolve(__dirname, '../../firebase-credentials.json');
+        const credentialsJson = process.env.FIREBASE_CREDENTIALS;
+        if (!credentialsJson) {
+          throw new Error('Variável de ambiente FIREBASE_CREDENTIALS não definida.');
+        }
+
+        const serviceAccount = JSON.parse(credentialsJson);
 
         admin.initializeApp({
-          credential: admin.credential.cert(serviceAccountPath),
+          credential: admin.credential.cert(serviceAccount),
           storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         });
-        logger.info('Firebase Admin SDK inicializado com sucesso a partir do arquivo de credenciais.');
+
+        logger.info('Firebase Admin SDK inicializado com sucesso via variável de ambiente.');
       }
 
       db = admin.firestore();
       firebaseInitialized = true;
       resolve(db);
     } catch (error) {
-      logger.error('Erro ao inicializar Firebase Admin SDK a partir do arquivo de credenciais. Verifique se o arquivo "firebase-credentials.json" existe e está no formato correto.', error);
+      logger.error(
+        'Erro ao inicializar Firebase Admin SDK. Verifique a variável FIREBASE_CREDENTIALS.',
+        error
+      );
       logger.warn('A autenticação usará apenas JWT.');
       reject(error);
     }
@@ -35,7 +43,9 @@ export const initializeFirebase = (): Promise<admin.firestore.Firestore> => {
 };
 
 // Função para verificar um token do Firebase
-export const verifyFirebaseToken = async (token: string): Promise<admin.auth.DecodedIdToken | null> => {
+export const verifyFirebaseToken = async (
+  token: string
+): Promise<admin.auth.DecodedIdToken | null> => {
   if (!firebaseInitialized) {
     logger.warn('Tentativa de verificar token Firebase, mas o SDK não está inicializado');
     return null;
@@ -50,8 +60,4 @@ export const verifyFirebaseToken = async (token: string): Promise<admin.auth.Dec
   }
 };
 
-export {
-  admin,
-  db,
-  firebaseInitialized,
-};
+export { admin, db, firebaseInitialized };
